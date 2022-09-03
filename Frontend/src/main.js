7315d24ea2d678ec4233f1943a8953467f1c5b25
LukasSemler <semler.l04@htlwienwest.at>
2022-09-03 17:17:45 +0200

    Register Fertig

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue';
 import router from './router';
 import './assets/css/index.css';
 import { createPinia } from 'pinia';
+import axios from 'axios';
+
+//Base-URL axios
+if (location.origin === 'http://localhost:8080' || location.origin === 'http://localhost:5050') {
+  axios.defaults.baseURL = 'http://localhost:2410';
+}
 
 const app = createApp(App);
 
--- Frontend/src/views/RegisterView.vue
+++ Frontend/src/views/RegisterView.vue
@@ -1,4 +1,99 @@
 <template>
+  <!-- Duplicate Error -->
+  <div
+    aria-live="assertive"
+    class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+  >
+    <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
+      <!-- Notification panel, dynamically insert this into the live region when it needs to be displayed -->
+      <transition
+        enter-active-class="transform ease-out duration-300 transition"
+        enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+        enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <div
+          v-if="duplicateError"
+          class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5"
+        >
+          <div class="p-4">
+            <div class="flex items-start">
+              <div class="flex-shrink-0">
+                <XCircleIcon class="h-6 w-6 text-red-600" aria-hidden="true" />
+              </div>
+              <div class="ml-3 w-0 flex-1 pt-0.5">
+                <p class="text-sm font-medium text-gray-900">Error</p>
+                <p class="mt-1 text-sm text-gray-500">
+                  Es ist bereits ein User mit dieser Email registriert. Nimm bitte eine andere
+                </p>
+              </div>
+              <div class="ml-4 flex flex-shrink-0">
+                <button
+                  type="button"
+                  @click="duplicateError = false"
+                  class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  <span class="sr-only">Close</span>
+                  <XMarkIcon class="h-5 w-5" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
+  </div>
+
+  <!-- Error -->
+  <div
+    aria-live="assertive"
+    class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+  >
+    <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
+      <!-- Notification panel, dynamically insert this into the live region when it needs to be displayed -->
+      <transition
+        enter-active-class="transform ease-out duration-300 transition"
+        enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+        enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <div
+          v-if="error"
+          class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5"
+        >
+          <div class="p-4">
+            <div class="flex items-start">
+              <div class="flex-shrink-0">
+                <XCircleIcon class="h-6 w-6 text-red-600" aria-hidden="true" />
+              </div>
+              <div class="ml-3 w-0 flex-1 pt-0.5">
+                <p class="text-sm font-medium text-gray-900">Error</p>
+                <p class="mt-1 text-sm text-gray-500">
+                  Leider ist beim Erstellen ein Fehler aufgetreten. Bitte probiere es nocheinmal
+                  oder kontaktiere unseren Support
+                </p>
+              </div>
+              <div class="ml-4 flex flex-shrink-0">
+                <button
+                  type="button"
+                  @click="error = false"
+                  class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  <span class="sr-only">Close</span>
+                  <XMarkIcon class="h-5 w-5" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
+  </div>
+
   <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
       <img
@@ -255,7 +350,6 @@
       </div>
     </div>
   </div>
-  {{ selectedAvatar }}
 </template>
 
 <script setup>
@@ -269,14 +363,19 @@ import {
   RadioGroupLabel,
   RadioGroupOption,
 } from '@headlessui/vue';
-import { CheckCircleIcon } from '@heroicons/vue/20/solid';
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/vue/20/solid';
 import { InformationCircleIcon } from '@heroicons/vue/20/solid';
+import { XMarkIcon } from '@heroicons/vue/20/solid';
 
 // Imports für vuelidate
 import useValidate from '@vuelidate/core';
 import { required, email, minLength, sameAs } from '@vuelidate/validators';
 
 import axios from 'axios';
+
+// Variablen für die Error Messages
+let duplicateError = ref(false);
+let error = ref(false);
 
 // Variable für die Radio Buttons
 const spieler_trainer = [
@@ -337,19 +436,30 @@ const validator = useValidate(rules, state);
 async function register(e) {
   e.preventDefault();
 
-  console.log(validator);
   if (validator.value.$silentErrors.length == 0) {
-    // Daten an den Server schicken
-    const result = await axios.post('/register', {
-      vorname: state.vorname,
-      nachname: state.nachname,
-      email: state.email,
-      password: state.password,
-      type: spielerTrainerLists.value.title,
-      avatar_path: selectedAvatar.value.path,
-    });
-
-    console.log(result);
+    try {
+      // Daten an den Server schicken
+      const result = await axios.post('/register', {
+        vorname: state.vorname,
+        nachname: state.nachname,
+        email: state.email,
+        password: state.password,
+        type: spielerTrainerLists.value.title,
+        avatarPath: selectedAvatar.value.path,
+      });
+      // Ergebnis auswerten
+      if (result.status === 200) router.push('/login');
+    } catch (error) {
+      console.log(error.message);
+      if (error.message.includes('400')) {
+        duplicateError.value = true;
+        setTimeout(() => (duplicateError.value = false), 3000);
+      }
+      if (error.message.includes('500')) {
+        error.value = true;
+        setTimeout(() => (error.value = false), 3000);
+      }
+    }
   } else {
     console.log('fehler');
   }
